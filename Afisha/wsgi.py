a9218ac318a0cf20ceea520d7a6173f4cfe70b8b
Aisen23Git <aiseneldos@gmail.com>
2024-09-05 00:22:31 +0600

    First Commit(Eight HW ex1. Last HomeWork)

--- Afisha/wsgi.py
+++ Afisha/wsgi.py
@@ -14,6 +14,6 @@ from django.core.wsgi import get_wsgi_application
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'Afisha.settings')
 
 import dotenv
-dotenv.read_dotenv(os.path.join(os.path.dirname(os.path.dirname(__file__)), '.env'))
+dotenv.read_dotenv(os.path.join(os.path.dirname(os.path.dirname(__file__)),'.env'))
 
 application = get_wsgi_application()
